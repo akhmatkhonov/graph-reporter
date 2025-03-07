fbd7bff40e74336f4354b79f611ab0a2de210475
akhmatkhonov <67595248+akhmatkhonov@users.noreply.github.com>
2025-03-07 19:43:53 +0500

    Update 002.VHMGR_Types.sql

--- installation/002.VHMGR_Types.sql
+++ installation/002.VHMGR_Types.sql
@@ -1,23 +1,25 @@
-create or replace type cpath_pathtopop_link_int force as object (
+begin
+
+execute immediate 'create or replace type cpath_pathtopop_link_int force as object (
   link_id number,
   sites   tableofnum
-);
-/
+)';
 
-create or replace type cpath_pathtopop_link_type force as object (
+
+execute immediate 'create or replace type cpath_pathtopop_link_type force as object (
   market_id   number,
   pop_site_id number,
   link_id     number,
   site_id     number
-);
-/
-
-create or replace type cpath_pathtopop_link_table force
-as table of cpath_pathtopop_link_type;
-/
+)';
 
 
-create or replace type cpath_pathtopop_path_type force as object (
+execute immediate 'create or replace type cpath_pathtopop_link_table force
+as table of cpath_pathtopop_link_type';
+
+
+
+execute immediate 'create or replace type cpath_pathtopop_path_type force as object (
   market_id         number,
   pop_site_id       number,
   root_site_id      number,
@@ -32,15 +34,15 @@ create or replace type cpath_pathtopop_path_type force as object (
   bh_break_link_t1  number,
   bh_break_link_t2  number,
   bh_break_link_t3  number
-);
-/
-
-create or replace type cpath_pathtopop_path_table force
-as table of cpath_pathtopop_path_type;
-/
+)';
 
 
-create or replace type cpath_rpt_link_type force as object (
+execute immediate 'create or replace type cpath_pathtopop_path_table force
+as table of cpath_pathtopop_path_type';
+
+
+
+execute immediate 'create or replace type cpath_rpt_link_type force as object (
   site_1           number,
   site_2           number,
   link_id          number,
@@ -56,22 +58,22 @@ create or replace type cpath_rpt_link_type force as object (
   map
   member
   function MAPRPTLINKTYPE return varchar2
-);
-/
+)';
 
-create or replace type body cpath_rpt_link_type as
+
+execute immediate 'create or replace type body cpath_rpt_link_type as
   map member function MAPRPTLINKTYPE return varchar2 is
 begin
   return site_1 || site_2 || link_id;
 end maprptlinktype;
-end;
-/
+end';
 
-create or replace type cpath_rpt_link_table force
-as table of cpath_rpt_link_type;
-/
 
-create or replace type cpath_rpt_path force as object (
+execute immediate 'create or replace type cpath_rpt_link_table force
+as table of cpath_rpt_link_type';
+
+
+execute immediate 'create or replace type cpath_rpt_path force as object (
   sites                tableofnum,
   links                cpath_rpt_link_table,
   lvl                  number,
@@ -80,121 +82,117 @@ create or replace type cpath_rpt_path force as object (
   count_break_link_ar1 number,
   count_break_link_ar2 number,
   count_break_link_sr  number
-);
+)';
+
+
+execute immediate 'create or replace type cpath_rpt_path_table force
+as table of cpath_rpt_path';
 /
 
-create or replace type cpath_rpt_path_table force
-as table of cpath_rpt_path;
-/
-
-create or replace type cpath_telco_type force as object (
+execute immediate 'create or replace type cpath_telco_type force as object (
   pop_site_id  number,
   path_id      number,
   site_id      number,
   link_id      number,
   order_number number
-);
-/
+)';
 
-create or replace type cpath_telco_table force
-as table of cpath_telco_type;
-/
 
-create or replace type t_cpath_legacynv_aug force as object (
+execute immediate 'create or replace type cpath_telco_table force
+as table of cpath_telco_type';
+
+
+execute immediate 'create or replace type t_cpath_legacynv_aug force as object (
   root_id    number,
   site_id    number,
   augment_id number
-);
-/
+)';
 
-create or replace type list_cpath_legacynv_aug force
-as table of t_cpath_legacynv_aug;
-/
 
-create or replace type t_cpath_legacynv_link force as object (
+execute immediate 'create or replace type list_cpath_legacynv_aug force
+as table of t_cpath_legacynv_aug';
+
+
+execute immediate 'create or replace type t_cpath_legacynv_link force as object (
   link_id   number,
   site_a_id number,
   site_z_id number
-);
-/
-
-create or replace type list_cpath_legacynv_link force
-as table of t_cpath_legacynv_link;
-/
+)';
 
 
-create or replace type t_cpath_legacynv_site force as object (
+execute immediate 'create or replace type list_cpath_legacynv_link force
+as table of t_cpath_legacynv_link';
+
+
+execute immediate 'create or replace type t_cpath_legacynv_site force as object (
   site_id number,
   cand_id number,
   core_id number
-);
-/
+)';
 
-create or replace type list_cpath_legacynv_site force
-as table of t_cpath_legacynv_site;
-/
 
-create or replace type t_fring_aug force as object (
+execute immediate 'create or replace type list_cpath_legacynv_site force
+as table of t_cpath_legacynv_site';
+
+
+execute immediate 'create or replace type t_fring_aug force as object (
   root_id    number,
   site_id    number,
   augment_id number
-);
-/
-
-create or replace type list_fring_aug force
-as table of t_fring_aug;
-/
+)';
 
 
-create or replace type t_fring_link force as object (
+execute immediate 'create or replace type list_fring_aug force
+as table of t_fring_aug';
+
+
+
+execute immediate 'create or replace type t_fring_link force as object (
   link_id       number,
   site_a_id     number,
   site_z_id     number,
   fiber_ring_id number
-);
-/
+)';
 
-create or replace type list_fring_link force
-as table of t_fring_link;
-/
 
-create or replace type t_fring_site force as object (
+execute immediate 'create or replace type list_fring_link force
+as table of t_fring_link';
+
+
+execute immediate 'create or replace type t_fring_site force as object (
   site_id number,
   cand_id number,
   core_id number
-);
-/
+)';
 
-create or replace type list_fring_site force
-as table of t_fring_site;
-/
 
-create or replace type list_id force
-is table of number;
-/
+execute immediate 'create or replace type list_fring_site force
+as table of t_fring_site';
 
-create or replace type t_sprint_clrpath force as object (
+  
+execute immediate 'create or replace type t_sprint_clrpath force as object (
   pop_site_id   number,
   root_id       number,
   path_id       number,
   ord_num       number,
   site_id       number,
   report_run_id number
-);
-/
+)';
 
-create or replace type list_sprint_clrpath force
-as table of t_sprint_clrpath;
-/
 
-create or replace type t_sprint_clrpath_admin force as object (
+execute immediate 'create or replace type list_sprint_clrpath force
+as table of t_sprint_clrpath';
+
+
+execute immediate 'create or replace type t_sprint_clrpath_admin force as object (
   param_name  varchar2(100),
   param_value varchar2(1000)
-);
-/
+)';
 
-create or replace type list_sprint_clrpath_admin force
-as table of t_sprint_clrpath_admin;
-/
+
+execute immediate 'create or replace type list_sprint_clrpath_admin force
+as table of t_sprint_clrpath_admin';
 
 dbms_output.put_line('Necessary TYPES have been created');
+
+end;
